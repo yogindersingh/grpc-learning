e1ff986cae0f4254f0be398f89b8bd215bc0ddd8
yogindersingh <yogindersingh757@gmail.com>
2024-07-08 11:32:03 +0530

    Project Setup, grpc-dependencies and proto compilation plugins

---
+++ src/test/java/com/learner/grpc_learning/GrpcLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.grpc_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
