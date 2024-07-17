d7e950372b6627326c003807a96441aa22dae60b
yogindersingh <yogindersingh757@gmail.com>
2024-07-17 23:12:30 +0530

    Server streaming example

--- src/main/java/com/learner/grpc_learning/P9/BankServiceImpl.java
+++ src/main/java/com/learner/grpc_learning/P9/BankServiceImpl.java
@@ -5,6 +5,8 @@ import com.learner.grpc_learning.proto.p9.BankAccounts;
 import com.learner.grpc_learning.proto.p9.BankRequest;
 import com.learner.grpc_learning.proto.p9.BankResponse;
 import com.learner.grpc_learning.proto.p9.BankServiceGrpc;
+import com.learner.grpc_learning.proto.p9.WithdrawalRequest;
+import com.learner.grpc_learning.proto.p9.WithdrawalResponse;
 import io.grpc.stub.StreamObserver;
 
 import java.util.List;
@@ -12,8 +14,7 @@ import java.util.Map;
 
 public class BankServiceImpl extends BankServiceGrpc.BankServiceImplBase {
 
-  public static Map<Integer, Integer> map = Map.of(12, 23, 14, 10, 120, 123);
-
+  private Map<Integer, Integer> map = Map.of(1, 1000, 2, 100, 3, 20000);
 
   @Override
   public void getAccountDetails(BankRequest request, StreamObserver<BankResponse> responseObserver) {
@@ -31,5 +32,16 @@ public class BankServiceImpl extends BankServiceGrpc.BankServiceImplBase {
     responseObserver.onNext(BankAccounts.newBuilder().addAllBankresponse(responseLost).build());
     responseObserver.onCompleted();
 
+  }
+
+  @Override
+  public void getWithdrawalAmount(WithdrawalRequest request, StreamObserver<WithdrawalResponse> responseObserver) {
+    Integer amount = request.getAmount();
+    Integer amountWidhrawed = 0;
+    for (int i = 0; i < amount/10; i++) {
+      amountWidhrawed=amountWidhrawed+amount/10;
+      responseObserver.onNext(WithdrawalResponse.newBuilder().setAmount(amount/10).build());
+    }
+    responseObserver.onCompleted();
   }
 }
