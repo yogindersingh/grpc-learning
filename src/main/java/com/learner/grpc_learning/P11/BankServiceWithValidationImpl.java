a935ed0d9d7fad7dd8b991ded75c53a4105fbb9e
yogindersingh <yogindersingh757@gmail.com>
2024-07-27 00:15:17 +0530

    Added input validation

---
+++ src/main/java/com/learner/grpc_learning/P11/BankServiceWithValidationImpl.java
@@ -0,0 +1,54 @@
+package com.learner.grpc_learning.P11;
+
+import com.learner.grpc_learning.P11.Repository.AccountRepository;
+import com.learner.grpc_learning.P11.Validator.RequestValidator;
+import com.learner.grpc_learning.proto.p11.BankRequest;
+import com.learner.grpc_learning.proto.p11.BankResponse;
+import com.learner.grpc_learning.proto.p11.BankServiceGrpc;
+import com.learner.grpc_learning.proto.p11.WithdrawalRequest;
+import com.learner.grpc_learning.proto.p11.WithdrawalResponse;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+
+public class BankServiceWithValidationImpl extends BankServiceGrpc.BankServiceImplBase {
+
+  @Override
+  public void getAccountDetails(BankRequest request,
+                                StreamObserver<com.learner.grpc_learning.proto.p11.BankResponse> responseObserver) {
+    RequestValidator.validateAccountId(request.getAccountNumber())
+        .map(Status::asRuntimeException)
+        .ifPresentOrElse(responseObserver::onError, () -> startFetchDetails(responseObserver, request));
+  }
+
+  void startFetchDetails(StreamObserver<BankResponse> responseObserver, BankRequest request) {
+    com.learner.grpc_learning.proto.p11.BankResponse bankResponse =
+        com.learner.grpc_learning.proto.p11.BankResponse.newBuilder().setAccountNumber(request.getAccountNumber())
+            .setBalance(AccountRepository.getAccountDetails(request.getAccountNumber())).build();
+    responseObserver.onNext(bankResponse);
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void getWithdrawalAmount(WithdrawalRequest request,
+                                  StreamObserver<com.learner.grpc_learning.proto.p11.WithdrawalResponse> responseObserver) {
+    RequestValidator.validateAccountId(request.getAccountNumber())
+        .or(() -> RequestValidator.validateAmountIsMultipleOfTen(request.getAmount()))
+        .or(() -> RequestValidator.validateAmountIsLessThanBalance(request.getAmount(), request.getAccountNumber()))
+        .map(Status::asRuntimeException)
+        .ifPresentOrElse(responseObserver::onError, () -> startWithdrawaingMoney(request, responseObserver));
+  }
+
+  private static void startWithdrawaingMoney(WithdrawalRequest request,
+                                             StreamObserver<WithdrawalResponse> responseObserver) {
+    int amount = request.getAmount();
+    Integer amountWidhrawed = 0;
+    for (int i = 0; i <  10; i++) {
+      amountWidhrawed = amountWidhrawed+amount / 10;
+      responseObserver.onNext(WithdrawalResponse.newBuilder().setAmount(amount / 10).build());
+    }
+    AccountRepository.updateAccountDetails(request.getAccountNumber(),
+        AccountRepository.getAccountDetails(request.getAccountNumber())-amountWidhrawed);
+    responseObserver.onCompleted();
+  }
+
+}
